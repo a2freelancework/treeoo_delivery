455a9d6011a472c293178dbc8fafba390496d5f8
Ananthu-calletic <ananthubsocial@gmail.com>
2023-09-28 15:14:34 +0530

    first_commit

---
+++ android/app/src/main/kotlin/com/example/treeo_delivery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.treeo_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/reusable/reusable_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const whiteColor = Color(0xffffffff);
+const blackColor = Color(0xff000000);
+const extralightgreen = Color(0xffCDE0C9);
+const lightgreen = Color(0xff68B2A0);
+const darkgreen = Color(0xff2c6975);
+const textblack = Color.fromARGB(255, 16, 18, 19);
+const peahcream = Color(0xffefe7da);
+
+const String Appname = "Treeoo";
